50ac95d46ec416469aacce275c18360b776afa57
Ahmed <ahmedashrafofficial0@gmail.com>
2021-08-05 23:00:38 +0200

    implement library to get rxjava from retrofit

--- FakeDownloadApp/app/src/main/java/com/codeprecious/fakedownloadapp/di/RetrofitModule.kt
+++ FakeDownloadApp/app/src/main/java/com/codeprecious/fakedownloadapp/di/RetrofitModule.kt
@@ -5,7 +5,9 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import io.reactivex.rxjava3.plugins.RxJavaPlugins
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
@@ -18,6 +20,7 @@ object RetrofitModule {
     fun provideRetrofitApi(): RetrofitApi {
         return Retrofit.Builder().baseUrl(RetrofitApi.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .build().create(RetrofitApi::class.java)
     }
 }
