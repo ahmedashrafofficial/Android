e47040af20afcf78e7983051652903f044b2089f
Ahmed <ahmedashrafofficial0@gmail.com>
2021-08-05 20:56:25 +0200

    Create Repository
    creating repository that controls functions in and out
    also injecting the repository and calling retrofitApi

---
+++ FakeDownloadApp/app/src/main/java/com/codeprecious/fakedownloadapp/repository/Repository.kt
@@ -0,0 +1,13 @@
+package com.codeprecious.fakedownloadapp.repository
+
+import com.codeprecious.fakedownloadapp.retrofit.RetrofitApi
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class Repository @Inject constructor(
+    private val retrofitApi: RetrofitApi
+) {
+
+    
+}
