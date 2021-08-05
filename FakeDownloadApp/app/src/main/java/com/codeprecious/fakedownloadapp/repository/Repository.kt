2f867034bd50f595aa2a778d31bbb96d874ea026
Ahmed <ahmedashrafofficial0@gmail.com>
2021-08-05 20:59:37 +0200

    Update repository
    building function that gets data from url using retrofit

--- FakeDownloadApp/app/src/main/java/com/codeprecious/fakedownloadapp/repository/Repository.kt
+++ FakeDownloadApp/app/src/main/java/com/codeprecious/fakedownloadapp/repository/Repository.kt
@@ -1,6 +1,8 @@
 package com.codeprecious.fakedownloadapp.repository
 
+import com.codeprecious.fakedownloadapp.model.Data
 import com.codeprecious.fakedownloadapp.retrofit.RetrofitApi
+import io.reactivex.rxjava3.core.Flowable
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -9,5 +11,7 @@ class Repository @Inject constructor(
     private val retrofitApi: RetrofitApi
 ) {
 
-    
+    fun getData(): Flowable<List<Data>> {
+        return retrofitApi.getFakeData()
+    }
 }
