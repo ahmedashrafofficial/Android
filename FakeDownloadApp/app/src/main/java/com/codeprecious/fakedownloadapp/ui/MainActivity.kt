cf2b4be3ee47c30c3ba438333ffab63470949021
Ahmed <ahmedashrafofficial0@gmail.com>
2021-08-05 20:31:32 +0200

    insert new packages
    to make our code clean and organizable, we create packages

--- FakeDownloadApp/app/src/main/java/com/codeprecious/fakedownloadapp/ui/MainActivity.kt
+++ FakeDownloadApp/app/src/main/java/com/codeprecious/fakedownloadapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.codeprecious.fakedownloadapp
+package com.codeprecious.fakedownloadapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codeprecious.fakedownloadapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
