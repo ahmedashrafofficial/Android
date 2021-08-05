9699903f71bd4c8549ba2e79c3fdc10ed0a4ec17
Ahmed <ahmedashrafofficial0@gmail.com>
2021-08-05 22:30:07 +0200

    create instance of viewModel

--- FakeDownloadApp/app/src/main/java/com/codeprecious/fakedownloadapp/ui/MainActivity.kt
+++ FakeDownloadApp/app/src/main/java/com/codeprecious/fakedownloadapp/ui/MainActivity.kt
@@ -2,13 +2,21 @@ package com.codeprecious.fakedownloadapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.codeprecious.fakedownloadapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: ViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(ViewModel::class.java)
+
     }
 }
