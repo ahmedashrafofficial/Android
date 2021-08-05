efa059e3ad25dfbf6a353b60b03b831aa8b34309
Ahmed <ahmedashrafofficial0@gmail.com>
2021-08-05 20:41:47 +0200

    prepare retrofit api
    Prepare retrofit api which gets data from the url

--- FakeDownloadApp/app/src/main/java/com/codeprecious/fakedownloadapp/retrofit/RetrofitApi.kt
+++ FakeDownloadApp/app/src/main/java/com/codeprecious/fakedownloadapp/retrofit/RetrofitApi.kt
@@ -1,9 +1,14 @@
 package com.codeprecious.fakedownloadapp.retrofit
 
+import retrofit2.http.GET
+
 
 interface RetrofitApi {
 
-    companion object{
+    companion object {
         const val BASE_URL = "https://nagwa.free.beeceptor.com/"
     }
+
+    @GET("movies")
+    fun getFakeData()
 }
