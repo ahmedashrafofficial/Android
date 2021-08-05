ef35ccf8fb3e6b1fbd4ce7d8ad1ac07715eef35f
Ahmed <ahmedashrafofficial0@gmail.com>
2021-08-05 22:26:49 +0200

    Update Repository
    gets data as a flowable and convert it to livedata

--- FakeDownloadApp/app/src/main/java/com/codeprecious/fakedownloadapp/repository/Repository.kt
+++ FakeDownloadApp/app/src/main/java/com/codeprecious/fakedownloadapp/repository/Repository.kt
@@ -1,8 +1,11 @@
 package com.codeprecious.fakedownloadapp.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.LiveDataReactiveStreams
 import com.codeprecious.fakedownloadapp.model.Data
 import com.codeprecious.fakedownloadapp.retrofit.RetrofitApi
 import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -11,7 +14,11 @@ class Repository @Inject constructor(
     private val retrofitApi: RetrofitApi
 ) {
 
-    fun getData(): Flowable<List<Data>> {
-        return retrofitApi.getFakeData()
+    fun getData(): LiveData<List<Data>> {
+        return LiveDataReactiveStreams.fromPublisher(
+            retrofitApi.getFakeData()
+                .subscribeOn(Schedulers.io())
+        )
+
     }
 }
